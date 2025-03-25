468dbdb85a20cc784d54dce70b415b3c64a26869
dueiras <dueiras@gmail.com>
2025-03-25 17:53:24 -0300

    Add timer to launch waypoints

--- humble_ws/src/navigation/launch/isaacsim_navigation.launch.py
+++ humble_ws/src/navigation/launch/isaacsim_navigation.launch.py
@@ -7,7 +7,24 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessIO
 
+def execute_second_node_if_condition_met(event, second_node_action):
+    """
+    Function taken from NVIDIA as a way to just launch ROS is isaacsim loads
+    Otherwise nav2 goes time out
+    """
+    output = event.text.decode().strip()
+    # Look for fully loaded message from Isaac Sim. Only applicable in Gui mode.
+    if "Stage loaded and simulation is playing." in output:
+        # Log a message indicating the condition has been met
+        print("Condition met, launching the second node.")
+        
+        # If Nav2 takes additional time to initialize, uncomment the lines below to add a delay of 10 seconds (or any desired duration) before launching the second_node_action
+        import time
+        time.sleep(10)
+        return second_node_action
 
 def generate_launch_description():
 
@@ -44,6 +61,22 @@ def generate_launch_description():
     nav2_bringup_launch_dir = os.path.join(get_package_share_directory("nav2_bringup"), "launch")
 
     rviz_config_dir = os.path.join(get_package_share_directory("navigation"), "rviz2", "global_navigation.rviz")
+
+    ld_automatic_goal = Node(
+                        name="set_navigation_goals",
+                        package="navigation",
+                        executable="navigate_through_poses",
+                        parameters=[
+                            {
+                                "iteration_count": 3,
+                                "action_server_name": "navigate_through_poses",
+                                "obstacle_search_distance_in_meters": 0.2,
+                                "goal_text_file_path": goal_text_file,
+                                "initial_pose": [2.4, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                            }
+                        ],
+                        output="screen",
+                    )
 
     return LaunchDescription(
         [
@@ -84,7 +117,13 @@ def generate_launch_description():
                     }
                 ],
                 output="screen",
-            )
+            ),
+            # Launch automatic goal generator node when Isaac Sim has finished loading.
+            RegisterEventHandler(
+                OnProcessIO(
+                    on_stdout=lambda event: execute_second_node_if_condition_met(event, ld_automatic_goal)
+                )
+            ),
 
         ]
     )
