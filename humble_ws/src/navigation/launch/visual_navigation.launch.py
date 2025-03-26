568e26f874327f8f955e53a8c316b75a427773eb
dueiras <dueiras@gmail.com>
2025-03-25 22:52:03 -0300

    add visual nagivation

---
+++ humble_ws/src/navigation/launch/visual_navigation.launch.py
@@ -0,0 +1,80 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration("use_sim_time", default="True")
+
+    map_dir = LaunchConfiguration(
+        "map",
+        default=os.path.join(
+            get_package_share_directory("navigation"), "maps", "extended_outdoor_navigation.yaml"
+        ),
+    )
+
+    param_dir = LaunchConfiguration(
+        "params_file",
+        default=os.path.join(
+            get_package_share_directory("navigation"), "params", "global_navigation_params.yaml"
+        ),
+    )
+
+    goal_text_file = LaunchConfiguration(
+        "goal_text_file_path",
+        default=os.path.join(get_package_share_directory("navigation"), "params", "goals.txt"),
+    )
+
+
+    nav2_bringup_launch_dir = os.path.join(get_package_share_directory("nav2_bringup"), "launch")
+
+    rviz_config_dir = os.path.join(get_package_share_directory("navigation"), "rviz2", "global_navigation.rviz")
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument("map", default_value=map_dir, description="Full path to map file to load"),
+            DeclareLaunchArgument(
+                "params_file", default_value=param_dir, description="Full path to param file to load"
+            ),
+            DeclareLaunchArgument(
+                "use_sim_time", default_value="true", description="Use simulation (Omniverse Isaac Sim) clock if true"
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(os.path.join(nav2_bringup_launch_dir, "rviz_launch.py")),
+                launch_arguments={"namespace": "", "use_namespace": "False","rviz_config": rviz_config_dir}.items(),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([nav2_bringup_launch_dir, "/bringup_launch.py"]),
+                launch_arguments={"map": map_dir, "use_sim_time": use_sim_time, "params_file": param_dir}.items(),
+            ),
+            Node(
+                name="set_navigation_goals",
+                package="navigation",
+                executable="navigate_through_poses",
+                parameters=[
+                    {
+                        "iteration_count": 3,
+                        "action_server_name": "navigate_through_poses",
+                        "obstacle_search_distance_in_meters": 0.2,
+                        "goal_text_file_path": goal_text_file,
+                        "initial_pose": [2.4, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                    }
+                ],
+                output="screen",
+            ),
+
+            Node(
+                name="visual_local_navigation",
+                package="perception",
+                executable="visual_navigation",
+                output="screen",
+            )
+
+        ]
+    )
